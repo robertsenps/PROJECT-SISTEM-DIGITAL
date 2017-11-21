6472ede6c6058435c5f401f519cc35db901a3331
robertsenps <robertsenputrasugianto@gmail.com>
2017-11-22 00:04:45 +0700

    RPS
    Udah bisa ke compile. Tapi dalemnya color rom nya ada output hit buat nentuin kena/ tidak

--- color_rom_vhd.vhd
+++ color_rom_vhd.vhd
@@ -9,13 +9,15 @@ ENTITY color_rom_vhd  IS
 		jump 			: IN STD_LOGIC;
 		reset 			: IN STD_LOGIC;
 		start			: IN STD_LOGIC;
-		airborne		: IN STD_LOGIC;
+		random 			: IN STD_LOGIC;
 	    KEY		        : IN STD_LOGIC_VECTOR( 1 DOWNTO 0 );
 	    i_pixel_column  : IN STD_LOGIC_VECTOR( 9 DOWNTO 0 );
 	    i_pixel_row     : IN STD_LOGIC_VECTOR( 9 DOWNTO 0 );
 	    o_red           : OUT STD_LOGIC_VECTOR( 7 DOWNTO 0 );
 	    o_green         : OUT STD_LOGIC_VECTOR( 7 DOWNTO 0 );
-	    o_blue          : OUT STD_LOGIC_VECTOR( 7 DOWNTO 0 ));
+	    o_blue          : OUT STD_LOGIC_VECTOR( 7 DOWNTO 0 );
+	    hit				: OUT STD_LOGIC);
+	    
 END color_rom_vhd; 
 
 ARCHITECTURE behavioral OF color_rom_vhd  IS 
@@ -35,10 +37,10 @@ BEGIN
 CLK_50HZ : clockdiv
 PORT MAP (
 	CLK		=> CLOCK_50,
-	div     => 500000;
+	div     => 500000,
 	DIVOUT	=> clock_50hz);
 
-PROCESS(i_pixel_row,i_pixel_column, M_SIG, B_SIG ,clock_50hz, jump, reset,airborne)
+PROCESS(i_pixel_row,i_pixel_column, M_SIG, B_SIG ,clock_50hz, jump, reset)
 
 -- KONDISI AWAL :
 	-- USER :
@@ -58,8 +60,7 @@ PROCESS(i_pixel_row,i_pixel_column, M_SIG, B_SIG ,clock_50hz, jump, reset,airbor
 	CONSTANT  BATAS_BAWAH   : INTEGER := 0;
 
 -- BATAS KOTAK OBSTACLE KETIKA BELUM ADA RINTANGAN :
-	CONSTANT  O_KI1  		: INTEGER := 700;
-	CONSTANT  O_KI2  		: INTEGER := 0;
+	CONSTANT  O_KI  		: INTEGER := 0;
 	
 -- DIMENSI KOTAK OBSTACLE :
 	VARIABLE O_KIRI			: INTEGER := 700;
@@ -70,8 +71,8 @@ PROCESS(i_pixel_row,i_pixel_column, M_SIG, B_SIG ,clock_50hz, jump, reset,airbor
 	VARIABLE U_BAWAH 		: INTEGER := 480;
 
 -- VARIABEL & KONSTANTA LAIN :
-	VARIABLE sentuh			: STD_LOGIC := 0;
-	
+	VARIABLE sentuh			: STD_LOGIC := '0';
+	VARIABLE gerak			: STD_LOGIC := '0';
 BEGIN
 
 	IF ( clock_50hz = '1' ) THEN
@@ -87,7 +88,7 @@ BEGIN
 			IF ( U_ATAS = BATAS_LONCAT) THEN
 				SENTUH := '1';
 			END IF;
-		ELSIF ((sentuh = '1') AND (U_BAWAH  <= BATAS_BAWAH) THEN
+		ELSIF ((sentuh = '1') AND (U_BAWAH  <= BATAS_BAWAH)) THEN
 			U_ATAS 	:= U_ATAS + 25;
 			U_BAWAH := U_BAWAH + 25 ;
 			IF ( U_BAWAH = BATAS_BAWAH) THEN
@@ -98,42 +99,34 @@ BEGIN
 			U_ATAS 	:= U_ATAS;
 			U_BAWAH := U_BAWAH;
 		END IF;
-
-		IF(reset = '1') THEN
 			
-
-			IF ( KEY(0)= '0') AND (R_D <= 479) THEN
-				R_D := R_D + trans;
-				R_T := R_T + trans;
-			ELSIF (KEY(1)= '0') AND (R_T > 0)  THEN
-				R_D := R_D - trans;
-				R_T := R_T - trans;
-			ELSIF ( KEY(2)= '0') AND (R_R <= 639) THEN
-				R_R := R_R + trans;
-				R_L := R_L + trans;
-			ELSIF (KEY(3)= '0') AND (R_L > 0)  THEN
-				R_R := R_R - trans;
-				R_L := R_L - trans;
-			ELSE
-				R_R := R_R;
-				R_L := R_L;
-				R_D := R_D;
-				R_T := R_T;
+		IF (((random = '1') OR (gerak = '1')) AND (O_KIRI >= O_KI)) THEN
+			gerak 	:= '1';
+			O_KIRI 	:= O_KIRI - 5;
+			O_KANAN := O_KANAN - 5;
+			IF (O_KIRI = O_KI) THEN
+				O_KIRI 	:= OBS_KIRI;
+				O_KANAN := OBS_KANAN;
+				gerak 	:= '0';
 			END IF;
 		END IF;
-	ELSE
-				R_R := R_R;
-				R_L := R_L;
-				R_D := R_D;
-				R_T := R_T;
+
+		IF ((USER_KANAN = O_KIRI AND U_BAWAH >= OBS_ATAS) OR U_BAWAH = OBS_ATAS) THEN
+			hit := '1';
+		END IF;
 	END IF;
 	
-  IF ((i_pixel_row >= R_T)  AND (i_pixel_row <R_D )  ) AND ((i_pixel_column >= R_L)  AND (i_pixel_column < R_R)  ) THEN M_TF1 <=  '1';
-  ELSE M_TF1 <=  '0';
+  IF ((i_pixel_row >= U_ATAS)  AND (i_pixel_row <= U_BAWAH )  ) AND ((i_pixel_column >= USER_KIRI )  AND (i_pixel_column <= USER_KANAN)  ) THEN B_SIG <=  '1';
+  ELSE B_SIG <=  '0';
   END IF;
 
-  IF (M_TF1 = '1') THEN  o_red <= X"FF"; o_green <= X"00"; o_blue <= X"00"; 
-  ELSE  o_red <= X"FF"; o_green <= X"FF"; o_blue <= X"FF";
+  IF ((i_pixel_row >= OBS_ATAS)  AND (i_pixel_row <= OBS_BAWAH )  ) AND ((i_pixel_column >= O_KIRI )  AND (i_pixel_column <= O_KANAN)  ) THEN M_SIG <=  '1';
+  ELSE M_SIG <=  '0';
+  END IF;
+
+  IF (B_SIG = '1') THEN  o_red <= X"00"; o_green <= X"00"; o_blue <= X"FF"; 
+  ELSIF (M_SIG = '1') THEN  o_red <= X"FF"; o_green <= X"00"; o_blue <= X"00";
+  ELSE o_red <= X"FF"; o_green <= X"FF"; o_blue <= X"FF";
   END IF;
  
 END PROCESS;
