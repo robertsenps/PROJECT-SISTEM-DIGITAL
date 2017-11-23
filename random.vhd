cfcb8aa8c7afeccc5f6cd02230ba70ac1aebeab6
robertsenps <robertsenputrasugianto@gmail.com>
2017-11-24 00:21:04 +0700

    Add files via upload
    Minor fix

--- random.vhd
+++ random.vhd
@@ -4,32 +4,19 @@ USE ieee.std_logic_arith.all;
 USE ieee.std_logic_unsigned.all;
 
 ENTITY random IS
-	PORT(	i_clk	: IN STD_LOGIC;
+	PORT(	i_clk	: IN BIT;
 			start	: IN STD_LOGIC;
 			random	: OUT STD_LOGIC
 		);
 END random;
 
 ARCHITECTURE behavioral OF random IS
-	SIGNAL clock		: BIT;
 	SIGNAL r_condition	: STD_LOGIC;
-	SIGNAL counter	: STD_LOGIC_VECTOR(6 DOWNTO 0);
+	SIGNAL counter		: STD_LOGIC_VECTOR(6 DOWNTO 0);
 	
-	COMPONENT clockdiv IS
-		PORT(	CLK	: IN STD_LOGIC;
-				div	: INTEGER;
-				DIVOUT	: BUFFER BIT
-			);
-	END COMPONENT;
-
 BEGIN
 
-	CLOCKSET : clockdiv
-		PORT MAP(	CLK => i_clk,
-					div	=> 500000,
-					DIVOUT	=> clock);
-	
-	PROCESS (clock)
+	PROCESS (i_clk,start)
 		CONSTANT max_count	: STD_LOGIC_VECTOR(6 DOWNTO 0) := "1100100";
 		CONSTANT min_count	: STD_LOGIC_VECTOR(6 DOWNTO 0) := "0000000";
 	
@@ -37,7 +24,7 @@ BEGIN
 		IF start = '1' THEN
 			r_condition <= '0';
 			counter <= min_count;
-		ELSIF (clock'EVENT AND clock = '1') THEN
+		ELSIF (i_clk'EVENT AND i_clk = '1') THEN
 			IF r_condition = '1' THEN
 				r_condition <= '0';
 				counter <= counter + 1;
